9b6a5f48149e8af7ed4266c5ec77bee6d8c41db5
Puy <mkhadaffy@users.noreply.github.com>
2018-01-12 21:09:47 +0700

    Update Talk.py

--- PUY/Api/Talk.py
+++ PUY/Api/Talk.py
@@ -20,9 +20,6 @@ class Talk:
   host = "gd2.line.naver.jp";
   port = 443;
 
-#  UA = "Line/7.4.7 iPad3,6 7.0.2"
- # LA = "IOSIPAD\t7.4.7\tiPhone OS\t7.0.2"
-
   UA = "Line/1.4.17"
   LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"
 
